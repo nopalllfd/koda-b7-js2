ce4b8ca9e413127c9dfb676392cc1a801c48a588
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-02-25 14:39:57 +0700

    Add negative price check to discountTotal function

--- minitask-2.js
+++ minitask-2.js
@@ -1,18 +1,22 @@
 function discountTotal(purchaseTotal) {
   let finalTotal = 0;
   if (typeof purchaseTotal === 'number') {
-    if (purchaseTotal > 500000) {
-      if (purchaseTotal > 1000000) {
-        finalTotal = purchaseTotal * 0.1;
-        const total = purchaseTotal - finalTotal;
-        console.log(`Total diskon anda adalah ${finalTotal}, harga total adalah ${total}, dari ${purchaseTotal}`);
-      } else {
-        finalTotal = purchaseTotal * 0.05;
-        const total = purchaseTotal - finalTotal;
-        console.log(`Total diskon anda adalah ${finalTotal}, harga total adalah ${total}, dari ${purchaseTotal}`);
-      }
+    if (purchaseTotal < 0) {
+      console.log(`Harga tidak bisa negatif`);
     } else {
-      console.log(`Tidak dapat diskon, total belanja adalah ${purchaseTotal}`);
+      if (purchaseTotal > 500000) {
+        if (purchaseTotal > 1000000) {
+          finalTotal = purchaseTotal * 0.1;
+          const total = purchaseTotal - finalTotal;
+          console.log(`Total diskon anda adalah ${finalTotal}, harga total adalah ${total}, dari ${purchaseTotal}`);
+        } else {
+          finalTotal = purchaseTotal * 0.05;
+          const total = purchaseTotal - finalTotal;
+          console.log(`Total diskon anda adalah ${finalTotal}, harga total adalah ${total}, dari ${purchaseTotal}`);
+        }
+      } else {
+        console.log(`Tidak dapat diskon, total belanja adalah ${purchaseTotal}`);
+      }
     }
   } else {
     console.log('Input harus number');
